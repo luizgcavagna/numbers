92e39311502137b061e083b72cea3fa28638a91d
Luiz G Cavagna <luizcavagna@gmail.com>
2026-04-16 15:31:47 -0300

    actions js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const form = document.querySelector("form");
+const span = document.querySelector("#results p>span");
+const resultsDiv = document.getElementById("results");
+const resultDiv = document.getElementById("result");
+const formDiv = document.getElementById("form-div");
 const number = document.getElementById("number");
 const of = document.getElementById("of");
 const to = document.getElementById("to");
+const checkbox = document.getElementById("checkbox");
+
+let times = 1;
 
 form.onsubmit = (event) => {
   event.preventDefault();
@@ -9,28 +16,51 @@ form.onsubmit = (event) => {
   const quantity = Number(number.value);
   const min = Number(of.value);
   const max = Number(to.value);
-  
-  if(!quantity || !min || !max )
-    return alert("Os dados precisam ser preenchidos!");
 
   if(min > max)
     return alert("De tem que ser menor do que até!")
 
   const results = draw(quantity, min, max);
-  console.log(results);
+
+  createItem(results);
+}
+
+function createItem(results) {
+  formDiv.classList.add("display-none");
+  resultsDiv.classList.remove("display-none");
+  resultDiv.innerHTML = '';
+  span.textContent = times;
+
+  results.forEach((result) => {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    item.textContent = result;
+
+    resultDiv.append(item);
+  });
+
+  times++;
+}
+
+function returnForm() {
+  formDiv.classList.remove("display-none");
+  resultsDiv.classList.add("display-none");
 }
 
 function draw(quantity, min, max){
-  let results = [];
+  const results = [];
 
   while(quantity > 0){
 
-    let result = mathRandom(min, max);
+    const result = mathRandom(min, max);
 
-    if(!results.includes(result)) {
+    if(Boolean(checkbox.value) === true && !results.includes(result)) 
       results.push(result);
-      quantity--;
-    } 
+      
+    else if(Boolean(checkbox.value) === false) 
+      results.push(result);
+  
+    quantity--;
   }
 
   return results;
@@ -39,3 +69,11 @@ function draw(quantity, min, max){
 function mathRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    checkbox.value = true;
+  } else {
+    checkbox.value = false;
+  }
+});
